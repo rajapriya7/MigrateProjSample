ba2f77cd98231cb6083e77960c44407346f70791
rajapriya7 <rajapriyakalimuthu@gmail.com>
2022-08-09 11:14:10 +0530

    My commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sample_003.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
